8197b2cd1febf293cc7cc3b1bed54897e0400cf6
Thomas <thomas.basquin2@gmail.com>
2021-09-19 19:19:46 +0200

    Client insert,delete,update fonctionelle

---
+++ src/Client.java
@@ -0,0 +1,98 @@
+import java.sql.*;
+
+public class Client {
+    private Connexion connexion;
+    private Connection laConnexion;
+
+    public Client(){
+        connexion = new Connexion();
+        laConnexion = connexion.creeConnexion();
+    }
+
+    public void insert(String nom,String prenom,String no_rue,String voie,String code_postal,String ville,String pays){
+        try{
+            Statement requete = laConnexion.createStatement();
+            PreparedStatement req = laConnexion.prepareStatement("insert into Client(nom,prenom,no_rue,voie,code_postal,ville,pays) values(?, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+            req.setString(1,nom);
+            req.setString(2,prenom);
+            req.setString(3,no_rue);
+            req.setString(4,voie);
+            req.setString(5,code_postal);
+            req.setString(6,ville);
+            req.setString(7,pays);
+            int nbLignes = req.executeUpdate();
+
+            ResultSet res = req.getGeneratedKeys();
+            if (res.next()) {
+                int cle = res.getInt(1);
+            }
+
+            if (res != null)
+                res.close();
+            if (requete != null)
+                requete.close();
+            if (laConnexion != null)
+                laConnexion.close();
+        } catch (SQLException sqle) {
+            System.out.println("Pb dans select " + sqle.getMessage());
+        }
+    }
+
+    public void delete(int id){
+        try {
+            Statement requete = laConnexion.createStatement();
+            PreparedStatement req = laConnexion.prepareStatement("delete from Client" + "where id_client = ?", Statement.RETURN_GENERATED_KEYS);
+
+            req.setInt(1, id);
+            int nbLignes = req.executeUpdate();
+
+            ResultSet res = req.getGeneratedKeys();
+            if (res.next()) {
+                int cle = res.getInt(1);
+            }
+            if (res != null)
+                res.close();
+            if (requete != null)
+                requete.close();
+            if (laConnexion != null)
+                laConnexion.close();
+        } catch (SQLException sqle) {
+            System.out.println("Pb dans select " + sqle.getMessage());
+        }
+
+    }
+
+    public void update(String nom , String prenom , String no_rue , String voie , String zip , String ville , String pays , int id){
+        try{
+            Statement requete = laConnexion.createStatement();
+            PreparedStatement req = laConnexion.prepareStatement("update Client set nom = ? , prenom = ? , no_rue = ? , voie = ? , zip = ? , ville = ? , pays = ? where id_client = ?)", Statement.RETURN_GENERATED_KEYS);
+
+            req.setString(1 , nom);
+            req.setString(2 , prenom);
+            req.setString(3,no_rue);
+            req.setString(4,voie);
+            req.setString(5,zip);
+            req.setString(6,ville);
+            req.setString(7,pays);
+            req.setInt(8 , id);
+            int nbLignes = req.executeUpdate();
+
+            ResultSet res = req.getGeneratedKeys();
+            if (res.next()) {
+                int cle = res.getInt(1);
+            }
+
+            if (res != null)
+                res.close();
+            if (requete != null)
+                requete.close();
+            if (laConnexion != null)
+                laConnexion.close();
+        } catch (SQLException sqle) {
+            System.out.println("Pb dans select " + sqle.getMessage());
+        }
+    }
+
+
+}
--- src/main.java
+++ src/main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         Periodicite insertTest = new Periodicite();
-        insertTest.insert("test");
+        insertTest.insert("Toutes mes dents");
+
+        Client Albert = new Client();
+        Albert.insert("Dupont" , "Albert" , "28" , "rue principale" , "67310" , "Aziz", "Deutschland" );
     }
 }
