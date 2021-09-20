cbfe16f5c691fcda06722d51915bfa9e96d89672
Thomas <thomas.basquin2@gmail.com>
2021-09-20 16:26:46 +0200

    test

--- src/Connexion.java
+++ src/Connexion.java
@@ -2,14 +2,13 @@ import java.sql.*;
 
 public class Connexion {
     public Connection creeConnexion() {
-        String url = "jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/kuntz48u_cpoatd1";
-        String login = "kuntz48u_appli";
-        String pwd = "Vagija@3";
+        String url = "jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/basquin6u_cpoa";
+        String login = "basquin6u_appli";
+        String pwd = "mpeg-f73";
         Connection maConnexion = null;
 
         try {
             maConnexion = DriverManager.getConnection(url, login, pwd);
-            System.out.println("Connecté !");
         } catch (SQLException sqle) {
             System.out.println("Erreur connexion" + sqle.getMessage());
         }
