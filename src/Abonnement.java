be9a7542c26f9bb26bd42034cdb341a450fccf3d
Alban Ktz <alban.ktz@gmail.com>
2021-09-20 15:59:00 +0200

    Tout est fait, sauf class Date dans abonnement

--- src/Abonnement.java
+++ src/Abonnement.java
@@ -39,6 +39,63 @@ public class Abonnement {
         }
     }
 
+    public void delete(int id_abonnement) {
+        try {
+            Statement requete = laConnexion.createStatement();
+            PreparedStatement req = laConnexion.prepareStatement("delete from Abonnement where id_abonnement=?", Statement.RETURN_GENERATED_KEYS);
+
+            req.setInt(1, id_abonnement);
+
+            int nbLignes = req.executeUpdate();
+
+            ResultSet res = req.getGeneratedKeys();
+            if (res.next()) {
+                int cle = res.getInt(1);
+            }
+
+            if (res != null) {
+                res.close();
+            } else if (requete != null) {
+                requete.close();
+            } else if (laConnexion != null) {
+                laConnexion.close();
+            }
+        } catch (SQLException sqle) {
+            System.out.println("Pb dans select" + sqle.getMessage());
+        }
+    }
+
+        public void update(int id_abonnement, Date date_debut, Date date_fin, int id_client, int id_revue) {
+            try{
+                Statement requete = laConnexion.createStatement();
+                PreparedStatement req = laConnexion.prepareStatement("UPDATE Abonnement SET date_debut = ?, date_fin = ?, id_client = ?, id_revue = ? WHERE id_abonnement = ?", Statement.RETURN_GENERATED_KEYS);
+
+                req.setDate(1, date_debut);
+                req.setDate(2, date_fin);
+                req.setInt(3, id_client);
+                req.setInt(4, id_revue);
+                req.setInt(5, id_abonnement);
+
+                int nbLignes = req.executeUpdate();
+
+                ResultSet res = req.getGeneratedKeys();
+                if (res.next()) {
+                    int cle = res.getInt(1);
+                }
+
+                if (res != null)
+                    res.close();
+                if (requete != null)
+                    requete.close();
+                if (laConnexion != null)
+                    laConnexion.close();
+
+
+            } catch (SQLException sqle) {
+                System.out.println("Pb dans select " + sqle.getMessage());
+        }
+    }
+
 
 
 }
--- src/main.java
+++ src/main.java
@@ -2,9 +2,5 @@ public class main {
     public static void main(String[] args) {
 
         Abonnement test = new Abonnement();
-        test.insert("2002/07/29", "2002/10/28", 1, 2);
-
-
-
     }
 }
