257ad5e750734d417b548cfd6cae6a773f90c9f6
Thomas <60144822+ThomasBasquin@users.noreply.github.com>
2021-10-03 20:15:07 +0200

    Delete src/Dao/Dao directory

--- src/Dao/Dao/Periodicite/DaoPeriodicite.java
+++
@@ -1,9 +0,0 @@
-package Dao.Periodicite;
-
-import Dao.Dao;
-
-import java.util.List;
-
-public interface DaoPeriodicite<Periodicite> extends Dao<Periodicite> {
-    List<Periodicite> getBylibelle(String libelle);
-}
