56626c274d3824a5eb87d9c45891d1772fb4e235
Alban <55509611+Alban-Ktz@users.noreply.github.com>
2021-10-10 10:35:54 +0200

    Add method normalize & 3 test methode(dont works)

--- TD3/src/Test/testAdresse.java
+++ TD3/src/Test/testAdresse.java
@@ -1,15 +1,39 @@
 package Test;
 
 import Metier.Adresse;
-import org.junit.jupiter.api.BeforeEach;
+import ProcessAdresse.*;
+import org.junit.jupiter.api.*;
+
+import static org.junit.Assert.*;
 
 public class testAdresse {
 
     private Adresse ad = new Adresse("belgium", "Moulins st les metz", "L-5700","3","av. aux Arènes");
+    ProcessAdresse procAd = new ProcessAdresse();
 
     @BeforeEach
     public void setUp() {
         this.ad = new Adresse("belgium", "Moulins st les metz", "L-5700","3","av. aux Arènes");
     }
 
+    // ======================
+    //      TEST PAYS
+    // ======================
+
+    @Test
+    public void testNormalizeLuxembourg() {
+        ad.setVille("letzebuerg");
+        assertEquals("Luxembourg", procAd.normalize(ad).getPays());
+    }
+
+    @Test
+    public void testNormalizeBelgique() {
+        assertEquals("Belgique", procAd.normalize(ad).getPays());
+    }
+
+    @Test
+    public void testNormalizeSuisse() {
+        ad.setVille("switzerland");
+        assertEquals("Suisse", procAd.normalize(ad).getPays());
+    }
 }
